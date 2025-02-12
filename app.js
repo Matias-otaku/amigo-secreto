e9e27453d155b5ee6d057db0fb37cc7de0ac2ce5
Nehemias Matias <nemisdonald@gmail.com>
2025-02-12 01:33:43 -0600

    Modificacion final

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const  inputAmigo = document.getElementById ("amigo");
+const listaAmigos = [];
+const ulListaAmigos = document.getElementById ("listaAmigos");
+const ulResultado = document.getElementById ("resultado");
+const limite = 5;
+
+
+function agregarAmigo(){
+    listaAmigos.push(inputAmigo.value);
+    if (listaAmigos.length >= limite + 1) {
+        limpiarCaja();
+        return;
+    }
+    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`
+    limpiarCaja();
+    return;
+}
+
+
+function sortearAmigo() {
+    const random = Math.floor(Math.random() * listaAmigos.length);
+    const amigoSecreto = listaAmigos[random];
+    ulResultado.innerHTML = `<li> El amigo secreto es: ${amigoSecreto}</li>`
+}
+
+function limpiarCaja() {
+    document.querySelector(`#amigo`).value = '';
+}
+
+
+
+
